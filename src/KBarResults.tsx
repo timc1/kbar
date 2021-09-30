d517ae3fb9eeaeee8914f3932ec0782fc3a47b38
Renan Ferreira <relferreira@gmail.com>
2021-09-30 12:50:41 -0300

    Update src/KBarResults.tsx
    Formatting code
    
    Co-authored-by: Tim <timchang.tcc@gmail.com>

--- src/KBarResults.tsx
+++ src/KBarResults.tsx
@@ -75,8 +75,7 @@ export default function KBarResults(props: KBarResultsProps) {
   const select = React.useCallback(() => {
     const action = matches[activeIndex];
 
-    if (!action)
-      return;
+    if (!action) return;
 
     if (action.perform) {
       action.perform();
