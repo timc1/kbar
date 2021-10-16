9a210abcde4d9602a397aa662aa2eb0fa063172a
Tim <timchang.tcc@gmail.com>
2021-10-16 17:36:09 -0400

    chore: page views (#63)

--- example/src/utils.tsx
+++ example/src/utils.tsx
@@ -1,3 +1,35 @@
+import * as React from "react";
+
 export function classnames(...args: (string | undefined | null)[]) {
   return args.filter(Boolean).join(" ");
+}
+
+export function useAnalytics() {
+  React.useEffect(() => {
+    const dev = window.location.host.includes("localhost");
+
+    if (!dev) {
+      const script = document.createElement("script");
+      script.src = "https://www.googletagmanager.com/gtag/js?id=G-TGC84TSLJZ";
+
+      document.body.appendChild(script);
+
+      script.onload = () => {
+        // @ts-ignore
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          // @ts-ignore
+          dataLayer.push(arguments);
+        }
+        // @ts-ignore
+        gtag("js", new Date());
+        // @ts-ignore
+        gtag("config", "G-TGC84TSLJZ");
+      };
+
+      return () => {
+        document.removeChild(script);
+      };
+    }
+  }, []);
 }
