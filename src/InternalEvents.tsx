4fd8d87104091344d06295e71089d70c1382ba22
Tom Moor <tom.moor@gmail.com>
2021-10-17 18:08:36 -0700

    fix: Allow suppression of kbar

--- src/InternalEvents.tsx
+++ src/InternalEvents.tsx
@@ -22,7 +22,11 @@ function useToggleHandler() {
 
   React.useEffect(() => {
     function handleKeyDown(event: KeyboardEvent) {
-      if ((event.metaKey || event.ctrlKey) && event.key === "k") {
+      if (
+        (event.metaKey || event.ctrlKey) &&
+        event.key === "k" &&
+        event.defaultPrevented === false
+      ) {
         event.preventDefault();
         query.setVisualState((vs) => {
           if (vs === VisualState.hidden || vs === VisualState.animatingOut) {
