adf32a7b68e2d5d632bc313688ef6f0f8653a58f
Tim <timothychang94@gmail.com>
2021-10-30 20:04:38 -0400

    fix: stop propagation on esc

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -123,6 +123,7 @@ const App = () => {
         },
       ]}
     >
+      <FakeModal />
       <SearchDocsActions />
       <KBarPortal>
         <KBarPositioner>
@@ -247,5 +248,27 @@ function HomeIcon() {
         fill="var(--foreground)"
       />
     </svg>
+  );
+}
+
+function FakeModal() {
+  const [showing, setShowing] = React.useState(true);
+
+  React.useEffect(() => {
+    function handler(e) {
+      if (e.key === "Escape") {
+        setShowing(false);
+      }
+    }
+    if (showing) {
+      window.addEventListener("keydown", handler);
+      return () => window.addEventListener("keydown", handler);
+    }
+  }, [showing]);
+
+  return showing ? (
+    <div>Fake modal showing</div>
+  ) : (
+    <button onClick={() => setShowing(true)}>Toggle fake modal</button>
   );
 }
--- src/InternalEvents.tsx
+++ src/InternalEvents.tsx
@@ -17,8 +17,9 @@ export default function InternalEvents() {
  * `useToggleHandler` handles the keyboard events for toggling kbar.
  */
 function useToggleHandler() {
-  const { query, options, visualState } = useKBar((state) => ({
+  const { query, options, visualState, hidden } = useKBar((state) => ({
     visualState: state.visualState,
+    hidden: state.visualState !== VisualState.showing,
   }));
 
   React.useEffect(() => {
@@ -37,7 +38,8 @@ function useToggleHandler() {
         });
       }
       if (event.key === "Escape") {
-        event.preventDefault();
+        if (!hidden) event.stopPropagation();
+
         query.setVisualState((vs) => {
           if (vs === VisualState.hidden || vs === VisualState.animatingOut) {
             return vs;
@@ -47,9 +49,9 @@ function useToggleHandler() {
       }
     }
 
-    window.addEventListener("keydown", handleKeyDown);
-    return () => window.removeEventListener("keydown", handleKeyDown);
-  }, [query]);
+    window.addEventListener("keydown", handleKeyDown, true);
+    return () => window.removeEventListener("keydown", handleKeyDown, true);
+  }, [query, hidden]);
 
   const timeoutRef = React.useRef<Timeout>();
   const runAnimateTimer = React.useCallback(
